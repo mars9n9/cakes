68ce15df5e847ac0814d8aace9e0444d83ac56bc
mars9n9 <mars9n9@gmail.com>
2024-10-13 15:55:49 +0200

    script

--- Convert-FolderContentToMarkdownTableOfContents.ps1
+++ Convert-FolderContentToMarkdownTableOfContents.ps1
@@ -1,8 +1,8 @@
-function Convert-FolderContentToMarkdownTableOfContents{
+function Convert-FolderContentToMarkdownTableOfContents {
     param (
         [string]$BaseFolder,
         [string]$FiletypeFilter,
-		[int]$Level = 0
+        [int]$Level = 0
     )
  
     $nl = [System.Environment]::NewLine
@@ -11,34 +11,44 @@ function Convert-FolderContentToMarkdownTableOfContents{
     $repoFolderStructure = Get-ChildItem -Path $BaseFolder -Directory | Where-Object Name -NotMatch "_site|pics|_posts|styles"
  
     foreach ($dir in ($repoFolderStructure | Sort-Object -Property Name)) {
-		
-		if ($Level -eq 0){
-		$suffix = "https://mars9n9.github.io/cakes/" + $($dir.Name)}
-		else {
-			$suffix = "https://mars9n9.github.io/cakes/" + $($BaseFolder.Split("\")[-1]) + "/" + $($dir.Name)}
-		
-        $TOC += "$(""  ""*$($Level))* [$($dir.Name)]($([uri]::EscapeUriString(""$suffix/$(""ix.md"".Replace("".md"", "".html""))""))) $nl"
-		$TOC += Convert-FolderContentToMarkdownTableOfContents -BaseFolder $dir.FullName -FiletypeFilter $FiletypeFilter -Level $($Level+1)
+        # Check if ix.md exists in the current directory
+        $ixFile = Get-ChildItem -Path $dir.FullName -Filter "ix.md" -ErrorAction SilentlyContinue
+
+        if ($ixFile) {
+            # If ix.md exists, create a link for the folder using the full path including the base folder
+            $relativePath = $dir.FullName.Replace((Get-Item $BaseFolder).Parent.FullName, "").TrimStart("\").Replace("\", "/")
+            $suffix = "https://mars9n9.github.io/cakes/$relativePath"
+            $TOC += "$(""  " * $Level)* [$($dir.Name)]($([uri]::EscapeUriString(""$suffix/ix.html"")))$nl"
+        } else {
+            # If ix.md does not exist, show the folder name as plain text
+            $TOC += "$(""  " * $Level)* $($dir.Name)$nl"
+        }
+
+        # Recursively call the function for subfolders
+        $TOC += Convert-FolderContentToMarkdownTableOfContents -BaseFolder $dir.FullName -FiletypeFilter $FiletypeFilter -Level $($Level+1)
+        
         $repoStructure = Get-ChildItem -Path $dir.FullName -Filter $FiletypeFilter
- 
-        foreach ($md in ($repoStructure | Where-Object Name -NotMatch "ix.md"| Sort-Object -Property Name)) {
-            $file_data = Get-Content "$($md.Directory.ToString())\$($md.Name)"  -Encoding UTF8
-			if ($file_data.count -gt 0){
-			$fileName = $file_data[0] -replace "# "}
-			else
-			{
-				$fileName = $($md.Name)
-			}
-			if ($Level -eq 0){
-			$suffix = "https://mars9n9.github.io/cakes" + $($md.Directory.ToString().Replace($BaseFolder, [string]::Empty)).Replace("\", "/")}
-			else {
-				$suffix = "https://mars9n9.github.io/cakes/" + $($BaseFolder.Split("\")[-1]) + $($md.Directory.ToString().Replace($BaseFolder, [string]::Empty)).Replace("\", "/")}
-            $TOC += "$(""  ""*$($Level+1))* [$fileName]($([uri]::EscapeUriString(""$suffix/$($md.Name.Replace(".md", ".html"))"")))$nl"
+
+        foreach ($md in ($repoStructure | Where-Object Name -NotMatch "ix.md" | Sort-Object -Property Name)) {
+            $file_data = Get-Content "$($md.Directory.ToString())\$($md.Name)" -Encoding UTF8
+            if ($file_data.count -gt 0) {
+                $fileName = $file_data[0] -replace "# "
+            } else {
+                $fileName = $($md.Name)
+            }
+            $relativePath = $md.Directory.ToString().Replace((Get-Item $BaseFolder).Parent.FullName, "").TrimStart("\").Replace("\", "/")
+            if ($Level -eq 0){
+                $suffix = "https://mars9n9.github.io/cakes" + $($md.Directory.ToString().Replace($BaseFolder, [string]::Empty)).Replace("\", "/")
+            } else {
+                $suffix = "https://mars9n9.github.io/cakes/$relativePath"   
+            }
+            $TOC += "$(""  " * ($Level + 1))* [$fileName]($([uri]::EscapeUriString(""$suffix/$($md.Name.Replace(".md", ".html"))"")))$nl"
         }
     }
  
     return $TOC
 }
+
 # Get the current directory
 $currentDirectory = Get-Location
-Convert-FolderContentToMarkdownTableOfContents -BaseFolder $currentDirectory -BaseURL "" -FiletypeFilter "*.md" | Out-File (Join-Path $currentDirectory "index.markdown") -Encoding UTF8
+Convert-FolderContentToMarkdownTableOfContents -BaseFolder $currentDirectory -FiletypeFilter "*.md" | Out-File (Join-Path $currentDirectory "index.markdown") -Encoding UTF8
